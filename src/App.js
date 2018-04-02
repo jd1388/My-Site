8f2254ba601f8aaa8f18b8af7d70bc095004554e
Jared Griffin <jgriffin@iastate.edu>
2018-04-02 08:49:47 -0500

    Added about section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 
 import Styles from './styles/App';
 
+const About = () => (
+    <div style={Styles.aboutContainer}>
+        <h1 style={Styles.hero}>Hello,</h1>
+        <p>
+            {`
+                My name is Jared Griffin. I'm a junior in Computer Engineering at
+                Iowa State University. Through side projects that I've worked on
+                with my friends and my apprenticeships, I've became a fluent web
+                developer with experience working on the entire stack. I have also
+                invested a lot of time in becoming a great leader, serving as
+                President of Iowa State's student branch of the Institute of
+                Electrical and Electronics Engineers and as a member of the
+                Electrical and Computer Engineering Department's Student Advisory
+                Committee. When I'm not knee-deep in side projects, I'm probably
+                playing video games or disc golf with friends.
+            `}
+        </p>
+    </div>
+);
+
 class App extends Component {
     render() {
         return (
             <div>
-                <h1 style={Styles.hero}>Hello</h1>
+                <About/>
             </div>
         );
     }
--- src/styles/App.js
+++ src/styles/App.js
@@ -1,13 +1,23 @@
 const blue = '#0984E3';
-// const black = '#232323';
+const black = '#232323';
 
 export default {
     hero: {
         fontSize: '120pt',
-        textAlign: 'center',
         color: blue,
         fontWeight: '400',
-        paddingTop: '10rem',
-        paddingBottom: '4rem'
-    }
+        paddingTop: 'calc(40vh - 120pt)',
+        alignSelf: 'center'
+    },
+    aboutContainer: {
+        fontFamily: 'Roboto, san-serif',
+        fontWeight: 300,
+        textAlign: 'justify',
+        color: black,
+        padding: '0 25% 5.5rem 25%',
+        height: '95vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
 };
