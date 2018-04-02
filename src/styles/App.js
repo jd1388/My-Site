2470346ee1bdf853b5a916d189bb1a7a56484dd6
Jared Griffin <jgriffin@iastate.edu>
2018-04-02 08:28:50 -0500

    Removed cra demo project stuff

---
+++ src/styles/App.js
@@ -0,0 +1,13 @@
+const blue = '#0984E3';
+// const black = '#232323';
+
+export default {
+    hero: {
+        fontSize: '120pt',
+        textAlign: 'center',
+        color: blue,
+        fontWeight: '400',
+        paddingTop: '10rem',
+        paddingBottom: '4rem'
+    }
+};
