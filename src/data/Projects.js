b704bcd223b8737cfd498f0b846bbe1ebb537c9e
Jared Griffin <jgriffin@iastate.edu>
2018-04-03 15:35:02 -0500

    Extracted data for project components to separate file

---
+++ src/data/Projects.js
@@ -0,0 +1,20 @@
+export const terra = {
+    name: 'Terra',
+    description: `
+        Terra is a platform for users to post services that they need done at their residences (lawncare, snow removal, etc.). 
+        After a user makes a post, another user can complete the tasks in the post, then get paid through Terra. Currently I'm 
+        working on this project with a group of friends. We are currently working on the first iteration of the project, which 
+        consists of users being able to create postings that other users can complete. The only services that we will handle 
+        initially is lawncare, but we will slowly add other services. I'm responsible for the frontend design. I've set up the 
+        Terra frontend to use React, and we use Redux for handling client-side data.
+    `
+};
+
+export const giphySearch = {
+    name: 'Giphy Search',
+    description: `
+        I created Giphy Search because I wanted to try some new React features, and I have wanted to try using a public API 
+        extensively for some time. My friends like their memes, so I had a group of people who could ask for features. Thus 
+        Giphy Search was born. Giphy Search wraps the Giphy API, specifically their search, trending, and random APIs.
+    `
+};
