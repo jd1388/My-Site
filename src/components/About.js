2ede60b47225300f9d1248ff3caebc55e9ca9d7c
Jared Griffin <jgriffin@iastate.edu>
2018-04-04 11:43:58 -0500

    Made my hello excited

--- src/components/About.js
+++ src/components/About.js
@@ -8,14 +8,14 @@ const About = ({ mobile }) => {
     if (mobile)
         return (
             <div style={Styles.mobileAboutContainer}>
-                <h1 style={Styles.mobileHero}>Hello,</h1>
+                <h1 style={Styles.mobileHero}>Hello!</h1>
                 <div style={Styles.sectionContent}>{introduction}</div>
             </div>
         );
 
     return (
         <div style={Styles.aboutContainer}>
-            <h1 style={Styles.hero}>Hello,</h1>
+            <h1 style={Styles.hero}>Hello!</h1>
             <div style={Styles.sectionContent}>{introduction}</div>
         </div>
     );
