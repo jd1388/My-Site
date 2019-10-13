a870b39866f06824ea7fc18588efacf4210f731d
Jared Griffin <jgriffin@iastate.edu>
2019-10-13 11:35:01 -0500

    Removed unused `mobile` prop from `Project` component

--- src/components/projects/project/Project.js
+++ src/components/projects/project/Project.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { ProjectContainer, ProjectName, ProjectDescription } from './styles';
 
-const Project = ({ projectInfo, mobile }) => {
+const Project = ({ projectInfo }) => {
   const { name, description } = projectInfo;
 
   return (
