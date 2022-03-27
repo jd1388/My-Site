73fe5329fe77fd36412cf24c4190d82b56fd2312
Jared Griffin <griffinjared1388@gmail.com>
2022-03-27 10:13:16 -0500

    Flushed out home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+    <h1>Hello!</h1>
+    <span>I'm Jared, and I write code</span>
+    <div class="links">
+        <a href="https://github.com/jd1388">Github</a>
+        <a href="https://www.linkedin.com/in/griffinjared/">LinkedIn</a>
+        <a href="mailto:jgriffin@onmail.com">Email</a>
+    </div>
+</section>
+
+<style>
+    section {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+        justify-content: center;
+        margin: 33vh 0 32px;
+        width: 100%;
+    }
+
+    h1 {
+        font-size: 96px;
+        margin: 0;
+    }
+
+    span {
+        font-size: 24px;
+        font-style: italic;
+    }
+
+    .links {
+        display: flex;
+        gap: 16px;
+    }
+</style>
--- src/routes/index.test.ts
+++ src/routes/index.test.ts
@@ -3,11 +3,40 @@ import { render, screen } from '@testing-library/svelte';
 import Homepage from './index.svelte';
 
 describe('home page', () => {
-    it('displays the welcome message', () => {
+    it('displays the hero message', () => {
         render(Homepage);
 
-        const welcomeMessage = screen.getByText('Welcome to SvelteKit');
+        const helloText = screen.getByText('Hello!');
+        const subheaderText = screen.getByText('I\'m Jared, and I write code');
 
-        expect(welcomeMessage).toBeVisible();
+        expect(helloText).toBeVisible();
+        expect(subheaderText).toBeVisible();
+    });
+
+    it('has a link to Github', () => {
+        render(Homepage);
+
+        const githubLink = screen.getByRole('link', { name: 'Github' });
+
+        expect(githubLink).toBeVisible();
+        expect(githubLink).toHaveAttribute('href', 'https://github.com/jd1388');
+    });
+
+    it('has a link to LinkedIn', () => {
+        render(Homepage);
+
+        const linkedinLink = screen.getByRole('link', { name: 'LinkedIn' });
+
+        expect(linkedinLink).toBeVisible();
+        expect(linkedinLink).toHaveAttribute('href', 'https://www.linkedin.com/in/griffinjared/');
+    });
+
+    it('has a link to my email', () => {
+        render(Homepage);
+
+        const emailLink = screen.getByRole('link', { name: 'Email' });
+
+        expect(emailLink).toBeVisible();
+        expect(emailLink).toHaveAttribute('href', 'mailto:jgriffin@onmail.com');
     });
 });
